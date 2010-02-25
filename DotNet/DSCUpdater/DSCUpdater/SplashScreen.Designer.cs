cc3b1dfbd83efddede24b123fb6b704aee5b90f6
jrubengb <j.ruben.gonzalez.baird@gmail.com>
2010-02-24 17:03:42 -0800

    DSC Updater commit based on recent updates

--- DotNet/DSCUpdater/DSCUpdater/SplashScreen.Designer.cs
+++ DotNet/DSCUpdater/DSCUpdater/SplashScreen.Designer.cs
@@ -29,9 +29,26 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            Infragistics.Win.Appearance appearance1 = new Infragistics.Win.Appearance();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SplashScreen));
             this.tmrSplashScreen = new System.Windows.Forms.Timer(this.components);
+            this.lblVersionInfo = new Infragistics.Win.Misc.UltraLabel();
             this.SuspendLayout();
+            // 
+            // lblVersionInfo
+            // 
+            appearance1.BackColor = System.Drawing.Color.Transparent;
+            appearance1.TextHAlignAsString = "Right";
+            this.lblVersionInfo.Appearance = appearance1;
+            this.lblVersionInfo.BorderStyleInner = Infragistics.Win.UIElementBorderStyle.None;
+            this.lblVersionInfo.BorderStyleOuter = Infragistics.Win.UIElementBorderStyle.None;
+            this.lblVersionInfo.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblVersionInfo.Location = new System.Drawing.Point(176, 236);
+            this.lblVersionInfo.Margin = new System.Windows.Forms.Padding(2);
+            this.lblVersionInfo.Name = "lblVersionInfo";
+            this.lblVersionInfo.Size = new System.Drawing.Size(160, 29);
+            this.lblVersionInfo.TabIndex = 0;
+            this.lblVersionInfo.Text = "Version X.X, January 1st, 2000";
             // 
             // SplashScreen
             // 
@@ -40,6 +57,7 @@
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.ClientSize = new System.Drawing.Size(362, 285);
             this.ControlBox = false;
+            this.Controls.Add(this.lblVersionInfo);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.MaximizeBox = false;
             this.MinimizeBox = false;
@@ -56,5 +74,6 @@
         #endregion
 
         private System.Windows.Forms.Timer tmrSplashScreen;
+        private Infragistics.Win.Misc.UltraLabel lblVersionInfo;
     }
 }
