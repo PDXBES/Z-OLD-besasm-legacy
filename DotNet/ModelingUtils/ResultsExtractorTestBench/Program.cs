9527415d8b7d1f38c617422d77cb4af99e7c87ba
jhb <jhb@localhost>
2010-04-05 16:46:34 -0700

    Refactored XPResults E tables with lower-case field names. Changed some E table datatypes from int32 to double.

--- DotNet/ModelingUtils/ResultsExtractorTestBench/Program.cs
+++ DotNet/ModelingUtils/ResultsExtractorTestBench/Program.cs
@@ -9,13 +9,14 @@ namespace ResultsExtractorTestBench
     {
         static void Main(string[] args)
         {
-            XPSWMMResults xpResults = new XPSWMMResults(@"\\Cassio\systems_planning\8063_CombinedFacPlan\Models\Alts\Beech_Essex\BEE_NP-Pipe\sim\25\BEE_NP-Pipe_FU_25.out");
+          //XPSWMMResults xpResults = new XPSWMMResults(@"C:\Data\Projects\41800023-1 BES_DataManagement\TGD_FU_2011\sim\25");
+          XPSWMMResults xpResults = new XPSWMMResults(@"C:\Data\Projects\41800023-1 BES_DataManagement\TGD_FU_2011\sim\25\TGD_FU_25_2011.out");
 
             TableE19DataSet.TableE19DataTable tableE19 = xpResults.GetTableE19();
 
             foreach (TableE19DataSet.TableE19Row row in tableE19)
             {
-                Console.WriteLine(row.nodeName);
+                Console.WriteLine(row.NodeName);
             }
 
             xpResults.WriteToAccessDatabase(@"C:\temp\XPSWMMResults.mdb");
