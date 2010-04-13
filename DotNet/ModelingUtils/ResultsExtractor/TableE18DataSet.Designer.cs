5a05b5a6ed1699523ed4e70caf86fa8ddc9ab169
jrubengb <j.ruben.gonzalez.baird@gmail.com>
2010-04-13 14:48:46 -0700

    Merge merges changes from rev 93 & 94 changesets to the working rev 95 branch tip

--- DotNet/ModelingUtils/ResultsExtractor/TableE18DataSet.Designer.cs
+++ DotNet/ModelingUtils/ResultsExtractor/TableE18DataSet.Designer.cs
@@ -260,7 +260,7 @@ namespace SystemsAnalysis.Modeling.ModelUtils.ResultsExtractor {
         [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Data.Design.TypedDataSetGenerator", "2.0.0.0")]
         [global::System.Serializable()]
         [global::System.Xml.Serialization.XmlSchemaProviderAttribute("GetTypedTableSchema")]
-        public partial class TableE18DataTable : global::System.Data.DataTable, global::System.Collections.IEnumerable {
+        public partial class TableE18DataTable : global::System.Data.TypedTableBase<TableE18Row> {
             
             private global::System.Data.DataColumn columnNodeName;
             
@@ -353,11 +353,6 @@ namespace SystemsAnalysis.Modeling.ModelUtils.ResultsExtractor {
             public TableE18Row FindByNodeName(string NodeName) {
                 return ((TableE18Row)(this.Rows.Find(new object[] {
                             NodeName})));
-            }
-            
-            [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-            public virtual global::System.Collections.IEnumerator GetEnumerator() {
-                return this.Rows.GetEnumerator();
             }
             
             [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
