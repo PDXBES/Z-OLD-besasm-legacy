ff106051b8a539b687c40a238e5f571a2bc9a35e
jhb <jhb@localhost>
2010-05-06 10:31:57 -0700

    Test

---
+++ DotNet/DataAccessTestBench/DataAccessTestBench/Form1.cs
@@ -0,0 +1,260 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using SystemsAnalysis.DataAccess;
+using SystemsAnalysis.Reporting.ReportLibraries;
+using SystemsAnalysis.Modeling.ModelUtils.ResultsExtractor;
+
+namespace DataAccessTestBench
+{
+  public partial class Form1 : Form
+  {
+    private SystemsAnalysis.DataAccess.StormwaterControlsDataSet scDS;
+
+    private RecommendedPlanReport rpReport;
+
+    public Form1()
+    {
+      InitializeComponent();
+
+      string testModelPath = @"C:\Data\Projects\41800023-1_BES_DataManagement\Beech_Essex\BEE_RP\";
+      //string testModelPath = @"\\Cassio\systems_planning\8063_CombinedFacPlan\Models\RP\Beech_Essex\BEE_RP\";
+
+      string testAlternativePath = @"C:\Data\Projects\41800023-1_BES_DataManagement\Beech_Essex\BEE_FU\alternatives\BEE_RP\";
+      //string testAlternativePath = @"\\Cassio\systems_planning\8063_CombinedFacPlan\Models\RP\Beech_Essex\BEE_FU\alternatives\BEE_RP";
+
+      string testSwmmOutput = @"C:\Data\Projects\41800023-1_BES_DataManagement\Beech_Essex\BEE_RP\sim\4S6\BEE_NP-Pipe_FU_4S6.out";
+      //string testSwmmOutput = @"\\Cassio\systems_planning\8063_CombinedFacPlan\Models\RP\Beech_Essex\BEE_RP\sim\4S6\BEE_RP_4S6.out";
+
+      rpReport = new RecommendedPlanReport();
+      Dictionary<string, ReportBase.Parameter> parameters = new Dictionary<string, ReportBase.Parameter>();
+      parameters.Add("ModelPath", new ReportBase.Parameter("ModelPath", testModelPath));
+      parameters.Add("AlternativePath", new ReportBase.Parameter("AlternativePath", testAlternativePath));
+      parameters.Add("SwmmOutputFile", new ReportBase.Parameter("SwmmOutputFile", testSwmmOutput));
+      rpReport.LoadAuxilaryData(parameters);
+
+    }
+
+    private void button1_Click(object sender, EventArgs e)
+    {
+      var query =
+        from icNode in scDS.ICNode
+        join icStreetTarget in scDS.ic_StreetTargets
+        on icNode.FacNode equals icStreetTarget.XPSWMM_Name
+        join altStreetTarget in scDS.AltStreetTargets
+        on icStreetTarget.icID equals altStreetTarget.ICID
+        join mdlSurfSc in scDS.mdl_SurfSC_ac
+        on icStreetTarget.surfSCID equals mdlSurfSc.SurfSCID
+        group mdlSurfSc by altStreetTarget.FocusArea into grpFocusArea
+        orderby grpFocusArea.Key
+        select new
+        {
+          FocusArea = grpFocusArea.Key,
+          NetIA = grpFocusArea.Sum(p => p.c_netIMPacres)
+        };
+
+      textBox1.AppendText("Infiltrate Stormwater\n");
+      textBox1.AppendText("------------------------------------\n");
+      foreach (var row in query)
+      {
+        textBox1.AppendText("Focus Area " + row.FocusArea + ": " + row.NetIA + "\n");
+      }
+
+      textBox1.AppendText("\n");
+      textBox1.AppendText("Protect and Improve Terrestrial Habitat\n");
+      textBox1.AppendText("------------------------------------\n");
+      var query2 =
+        from icNode in scDS.ICNode
+        join icStreetTarget in scDS.ic_StreetTargets
+        on icNode.FacNode equals icStreetTarget.XPSWMM_Name
+        join altStreetTarget in scDS.AltStreetTargets
+        on icStreetTarget.icID equals altStreetTarget.ICID
+        where icNode.Factype == "C" && icStreetTarget.constructed == 0
+        group icNode by altStreetTarget.FocusArea into grpFocusArea
+        orderby grpFocusArea.Key
+        select new
+        {
+          FocusArea = grpFocusArea.Key,
+          FacilityVolume = grpFocusArea.Sum(p => p.FacVolCuFt) / 0.75 / 43560,
+        };
+
+      foreach (var row in query2)
+      {
+        textBox1.AppendText("Focus Area " + row.FocusArea + ": " + row.FacilityVolume + "\n");
+      }
+    }
+
+    private void Form1_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    private void button2_Click(object sender, EventArgs e)
+    {
+      textBox1.Clear();
+
+      textBox1.AppendText("Infiltrate Stormwater Area\n");
+      textBox1.AppendText("------------------------------------\n");
+
+      var query =
+        from altStreetTargets in scDS.AltStreetTargets
+        group altStreetTargets by altStreetTargets.FocusArea into grpFocusArea
+        orderby grpFocusArea.Key
+        select new
+        {
+          FocusArea = grpFocusArea.Key
+        };
+
+      foreach (var row in query)
+      {
+        Dictionary<string, ReportBase.Parameter> parameters = new Dictionary<string, ReportBase.Parameter>();
+        parameters.Add("FocusArea", new ReportBase.Parameter("FocusArea", row.FocusArea));
+        double infiltratedArea = rpReport.InfiltrateStormwaterArea(parameters);
+
+        textBox1.AppendText("Focus Area: " + row.FocusArea + "; Infiltrated Area = " + infiltratedArea + "\n");
+      }
+
+      textBox1.AppendText("\n");
+      textBox1.AppendText("Protect and Improve Terrestrial Habitat Area\n");
+      textBox1.AppendText("------------------------------------\n");
+      foreach (var row in query)
+      {
+        Dictionary<string, ReportBase.Parameter> parameters = new Dictionary<string, ReportBase.Parameter>();
+        parameters.Add("FocusArea", new ReportBase.Parameter("FocusArea", row.FocusArea));
+        double infiltratedArea = rpReport.ProtectImproveTerrestrialHabitatArea(parameters);
+
+        textBox1.AppendText("Focus Area: " + row.FocusArea + "; Habitat Area = " + infiltratedArea + "\n");
+      }
+
+      textBox1.AppendText("\n");
+      textBox1.AppendText("Roof Planter Storage Volume\n");
+      textBox1.AppendText("------------------------------------\n");
+      foreach (var row in query)
+      {
+        Dictionary<string, ReportBase.Parameter> parameters = new Dictionary<string, ReportBase.Parameter>();
+        parameters.Add("FocusArea", new ReportBase.Parameter("FocusArea", row.FocusArea));
+        double stormwaterRemovalVol = rpReport.RoofPlanterStorageVolume(parameters);
+
+        textBox1.AppendText("Focus Area: " + row.FocusArea + "; Vol Remove: = " + stormwaterRemovalVol + "\n");
+      }
+
+      textBox1.AppendText("\n");
+      textBox1.AppendText("Roof Planter Infiltration Volume\n");
+      textBox1.AppendText("------------------------------------\n");
+      foreach (var row in query)
+      {
+        Dictionary<string, ReportBase.Parameter> parameters = new Dictionary<string, ReportBase.Parameter>();
+        parameters.Add("FocusArea", new ReportBase.Parameter("FocusArea", row.FocusArea));
+        double stormwaterRemovalVol = rpReport.RoofPlanterInfiltrationVolume(parameters);
+
+        textBox1.AppendText("Focus Area: " + row.FocusArea + "; Vol Remove: = " + stormwaterRemovalVol + "\n");
+      }
+
+      textBox1.AppendText("\n");
+      textBox1.AppendText("Roof Bio Storage Volume\n");
+      textBox1.AppendText("------------------------------------\n");
+      foreach (var row in query)
+      {
+        Dictionary<string, ReportBase.Parameter> parameters = new Dictionary<string, ReportBase.Parameter>();
+        parameters.Add("FocusArea", new ReportBase.Parameter("FocusArea", row.FocusArea));
+        double stormwaterRemovalVol = rpReport.RoofBioStorageVolume(parameters);
+
+        textBox1.AppendText("Focus Area: " + row.FocusArea + "; Vol Remove: = " + stormwaterRemovalVol + "\n");
+      }
+
+      textBox1.AppendText("\n");
+      textBox1.AppendText("Roof Bio Infiltration Volume\n");
+      textBox1.AppendText("------------------------------------\n");
+      foreach (var row in query)
+      {
+        Dictionary<string, ReportBase.Parameter> parameters = new Dictionary<string, ReportBase.Parameter>();
+        parameters.Add("FocusArea", new ReportBase.Parameter("FocusArea", row.FocusArea));
+        double stormwaterRemovalVol = rpReport.RoofBioInfiltrationVolume(parameters);
+
+        textBox1.AppendText("Focus Area: " + row.FocusArea + "; Vol Remove: = " + stormwaterRemovalVol + "\n");
+      }
+
+      textBox1.AppendText("\n");
+      textBox1.AppendText("Park Bio Storage Volume\n");
+      textBox1.AppendText("------------------------------------\n");
+      foreach (var row in query)
+      {
+        Dictionary<string, ReportBase.Parameter> parameters = new Dictionary<string, ReportBase.Parameter>();
+        parameters.Add("FocusArea", new ReportBase.Parameter("FocusArea", row.FocusArea));
+        double stormwaterRemovalVol = rpReport.ParkBioStorageVolume(parameters);
+
+        textBox1.AppendText("Focus Area: " + row.FocusArea + "; Vol Remove: = " + stormwaterRemovalVol + "\n");
+      }
+
+      textBox1.AppendText("\n");
+      textBox1.AppendText("Park Bio Infiltration Volume\n");
+      textBox1.AppendText("------------------------------------\n");
+      foreach (var row in query)
+      {
+        Dictionary<string, ReportBase.Parameter> parameters = new Dictionary<string, ReportBase.Parameter>();
+        parameters.Add("FocusArea", new ReportBase.Parameter("FocusArea", row.FocusArea));
+        double stormwaterRemovalVol = rpReport.ParkBioInfiltrationVolume(parameters);
+
+        textBox1.AppendText("Focus Area: " + row.FocusArea + "; Vol Remove: = " + stormwaterRemovalVol + "\n");
+      }
+
+
+    }
+
+    private void button3_Click(object sender, EventArgs e)
+    {
+      textBox1.Clear();
+
+      textBox1.AppendText("BSBR Count\n");
+      textBox1.AppendText("------------------------------------\n");
+
+      var query =
+       from focusAreas in rpReport.altCompilerDS.SP_RP_BSBR
+       group focusAreas by focusAreas.focus_area into grpFocusArea
+       orderby grpFocusArea.Key
+       select new
+       {
+         FocusArea = grpFocusArea.Key
+       };
+
+      Dictionary<string, ReportBase.Parameter> parameters;
+      int bsbrCount = 0;
+      foreach (var row in query)
+      {
+        parameters = new Dictionary<string, ReportBase.Parameter>();
+        parameters.Add("StormEvent", new ReportBase.Parameter("StormEvent", "25"));
+        parameters.Add("UseFlag", new ReportBase.Parameter("UseFlag", "14"));
+        parameters.Add("FocusArea", new ReportBase.Parameter("FocusArea", row.FocusArea));
+        bsbrCount = rpReport.BSBRCount(parameters);
+
+        textBox1.AppendText(row.FocusArea + "BSBR Count: " + bsbrCount.ToString() + "\n");
+      }
+      parameters = new Dictionary<string, ReportBase.Parameter>();
+      
+      parameters.Add("UseFlag", new ReportBase.Parameter("UseFlag", "14"));
+      parameters.Add("StormEvent", new ReportBase.Parameter("StormEvent", "25"));      
+      bsbrCount = rpReport.BSBRCount(parameters);
+
+      textBox1.AppendText("Total BSBR Delta: " + bsbrCount.ToString() + "\n");
+
+      int bsbrDelta = 0;
+      foreach (var row in query)
+      {
+        parameters = new Dictionary<string, ReportBase.Parameter>();
+        parameters.Add("StormEvent1", new ReportBase.Parameter("StormEvent1", "25"));
+        parameters.Add("UseFlag1", new ReportBase.Parameter("UseFlag1", "14"));
+        parameters.Add("StormEvent2", new ReportBase.Parameter("StormEvent2", "25"));
+        parameters.Add("UseFlag2", new ReportBase.Parameter("UseFlag2", "15"));
+        parameters.Add("FocusArea", new ReportBase.Parameter("FocusArea", row.FocusArea));
+        bsbrCount = rpReport.BSBRCount(parameters);
+
+        textBox1.AppendText(row.FocusArea + "BSBR Delta: " + bsbrCount.ToString() + "\n");
+      }                 
+    }
+  }
+}
