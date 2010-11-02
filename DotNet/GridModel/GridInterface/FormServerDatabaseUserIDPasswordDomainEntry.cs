bcd9194f6a46dfded0c1c622b7d015fee449a31c
ISSACG <ISSACG@localhost>
2010-11-02 15:17:13 -0700

    GridModel Schema added to repository, ServerDatabaseUserIDPasswordDomain form added to gridmodel.

---
+++ DotNet/GridModel/GridInterface/FormServerDatabaseUserIDPasswordDomainEntry.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SystemsAnalysis.Grid.GridAnalysis
+{
+    public partial class FormServerDatabaseUserIDPasswordDomainEntry : Form
+    {
+        public event EventHandler BigInfoFormClosed;
+        public string Database = "";
+        public string Server = "";
+        public string UserID = "";
+        public string Password = "";
+        public string Domain = "";
+        public bool useTrustedConnection = false;
+
+
+        public FormServerDatabaseUserIDPasswordDomainEntry()
+        {
+            InitializeComponent();
+
+            textBoxDatabase.Text = Database;
+            textBoxServer.Text = Server;
+            textBoxUserID.Text = UserID;
+            textBoxPassword.Text = Password;
+            textBoxDomain.Text = Domain;
+            checkBoxUseTrustedConnection.Checked = useTrustedConnection;
+        }
+
+        private void FormServerDatabaseUserIDPasswordDomainEntry_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            UserID = textBoxUserID.Text;
+            Password = textBoxPassword.Text;
+            Domain = textBoxDomain.Text;
+            Server = textBoxServer.Text;
+            Database = textBoxDatabase.Text;
+            useTrustedConnection = checkBoxUseTrustedConnection.Checked;
+        }
+
+        private void buttonOK_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
