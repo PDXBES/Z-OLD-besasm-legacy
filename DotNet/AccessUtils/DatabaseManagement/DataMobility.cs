103dcdaadf9b17088a86d0bea809e30e9bfb488e
ISSACG <@localhost>
2010-12-30 16:11:46 -0800

    Disintegrated some SQL related methods from accessHelper.  Moved those methods to SQLHelper and DataMobility.  Added classes SQLHelper and DataMobility.

---
+++ DotNet/AccessUtils/DatabaseManagement/DataMobility.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Access = Microsoft.Office.Interop.Access;
+using dao;
+using System.Reflection;
+//using Microsoft.Office.Interop.Access.Dao;
+using System.IO;
+using System.Data.SqlClient;
+using System.Data.OleDb;
+using System.Text;
+using System.Management;
+using SMO = Microsoft.SqlServer.Management.Smo;
+using System.Security.AccessControl;
+using Microsoft.SqlServer.Management.Smo.RegisteredServers;
+
+namespace SystemsAnalysis.Utils.AccessUtils
+{
+    class DataMobility
+    {
+    }
+}
