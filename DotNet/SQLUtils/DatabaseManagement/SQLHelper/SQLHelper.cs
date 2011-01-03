ae83e2f1913994a8e3806cf7625dcd8b5ac2ff69
ISSACG <@localhost>
2011-01-03 09:24:44 -0800

    Complete disintegration of AccessHelper and SQLHelper/DataMobility.

--- DotNet/SQLUtils/DatabaseManagement/SQLHelper/SQLHelper.cs
+++ DotNet/SQLUtils/DatabaseManagement/SQLHelper/SQLHelper.cs
@@ -4,7 +4,6 @@ using System.Collections.Generic;
 using Access = Microsoft.Office.Interop.Access;
 using dao;
 using System.Reflection;
-//using Microsoft.Office.Interop.Access.Dao;
 using System.IO;
 using System.Data.SqlClient;
 using System.Data.OleDb;
@@ -14,7 +13,8 @@ using SMO = Microsoft.SqlServer.Management.Smo;
 using System.Security.AccessControl;
 using Microsoft.SqlServer.Management.Smo.RegisteredServers;
 
-namespace SystemsAnalysis.Utils.AccessUtils
+
+namespace SQLHelper
 {
     class SQLHelper
     {
