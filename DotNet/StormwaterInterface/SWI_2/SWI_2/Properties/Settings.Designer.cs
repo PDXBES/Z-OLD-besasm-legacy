e81c0551251b8207a10750882dedb4dbf5c21d2d
ISSACG <@localhost>
2011-03-03 15:22:32 -0800

    SWI connection string should be user supplied.  These are the beginnings of codes that help do that.

--- DotNet/StormwaterInterface/SWI_2/SWI_2/Properties/Settings.Designer.cs
+++ DotNet/StormwaterInterface/SWI_2/SWI_2/Properties/Settings.Designer.cs
@@ -26,7 +26,7 @@ namespace SWI_2.Properties {
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
-        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=SIRTOBY;Initial Catalog=SWI;Integrated Security=SSPI;")]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=SIRTOBY;Initial Catalog=SWI;Integrated Security=True")]
         public string SANDBOXConnectionString {
             get {
                 return ((string)(this["SANDBOXConnectionString"]));
