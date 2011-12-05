2d9aaa02add3711104eb4ef05403c0be3d6d2062
Arnel Mandilag <arnel.mandilag@portlandoregon.gov>
2011-12-05 10:19:39 -0800

    Access Databases: EmgaatsCode, HydInitDSC (not sure what changed...)

---
+++ DotNet/AlternativesToolkit/AlternativeEngine/EngineSettings.cs
@@ -0,0 +1,108 @@
+﻿namespace SystemsAnalysis.ModelConstruction.AlternativesToolkit {
+
+
+    partial class AltEngineConfiguration
+    {
+        private string configPath;
+
+        public AltEngineConfiguration(string configPath)
+        {
+            this.configPath = configPath;
+            
+            ReadXml(configPath);            
+        }
+
+        public string UpdateVersionPatchMBX
+        {
+            get { return this.ProgramSettings[0].UpdateVersionPatchMBX; }
+        }
+
+
+        #region Manage Recently Used Files
+        public void UpdateBaseModelHistory(string baseModel)
+        {
+            string newHistory = baseModel;
+            string newDate = System.DateTime.Now.ToString("G");
+
+            int historyCount = System.Math.Min(
+                this.FileHistory[0].HistoryCount,
+                this.BaseModel.Count + 1);
+
+            for (int i = 0; i < historyCount; i++)
+            {
+                if (this.BaseModel.Count <= i)
+                {
+                    this.BaseModel.AddBaseModelRow("", "", this.FileHistory[0]);
+                }
+
+                AltEngineConfiguration.BaseModelRow historyRow;
+                historyRow = this.BaseModel[i];
+                string tempHistory = historyRow.IsNull("BaseModel_text")
+                    ? "" : historyRow.BaseModel_text;
+                string tempDate = historyRow.IsDateNull()
+                    ? "" : historyRow.Date;
+
+                if (tempHistory == baseModel)
+                {
+                    historyRow.Delete();
+                    historyCount--;
+                    i--;
+                    continue;
+                }
+
+                historyRow.BaseModel_text = newHistory;
+                historyRow.Date = newDate;
+
+                newHistory = tempHistory;
+                newDate = tempDate;
+            }
+            return;
+        }
+
+        public void UpdateOutputModelHistory(string outputModel)
+        {
+            string newHistory = outputModel;
+            string newDate = System.DateTime.Now.ToString("G");
+
+            int historyCount = System.Math.Min(
+                this.FileHistory[0].HistoryCount,
+                this.OutputModel.Count + 1);
+
+            for (int i = 0; i < historyCount; i++)
+            {
+                if (this.OutputModel.Count <= i)
+                {
+                    this.OutputModel.AddOutputModelRow("", "", this.FileHistory[0]);
+                }
+
+                AltEngineConfiguration.OutputModelRow historyRow;
+                historyRow = this.OutputModel[i];
+                string tempHistory = historyRow.IsNull("OutputModel_text")
+                    ? "" : historyRow.OutputModel_text;
+                string tempDate = historyRow.IsDateNull()
+                    ? "" : historyRow.Date;
+
+                if (tempHistory == outputModel)
+                {
+                    historyRow.Delete();
+                    historyCount--;
+                    i--;
+                    continue;
+                }
+
+                historyRow.OutputModel_text = newHistory;
+                historyRow.Date = newDate;
+
+                newHistory = tempHistory;
+                newDate = tempDate;
+            }
+            return;
+        }
+
+        #endregion
+
+
+        
+
+    }
+}
---
+++ DotNet/DataAccess/RetrofitsDataSet.cs
@@ -0,0 +1,72 @@
+﻿namespace SystemsAnalysis.DataAccess {
+    
+    
+    public partial class RetrofitsDataSet
+    {
+      partial class SiteAssessmentDataTable
+      {
+      }
+    
+      partial class SiteOpportunityDataTable
+      {
+      }
+    
+      public void InitRetroDataSet()
+      {
+        RetrofitsDataSetTableAdapters.SITE_ASSESSMENTTableAdapter siteAssessmentTA;
+        siteAssessmentTA = new RetrofitsDataSetTableAdapters.SITE_ASSESSMENTTableAdapter();
+        siteAssessmentTA.Fill(tableSITE_ASSESSMENT);
+
+        RetrofitsDataSetTableAdapters.ASSESSMENT_TYPETableAdapter assessmentTypeTA;
+        assessmentTypeTA = new RetrofitsDataSetTableAdapters.ASSESSMENT_TYPETableAdapter();
+        assessmentTypeTA.Fill(tableASSESSMENT_TYPE);
+
+        RetrofitsDataSetTableAdapters.SITE_OPPORTUNITYTableAdapter siteOpportunityTA;
+        siteOpportunityTA = new RetrofitsDataSetTableAdapters.SITE_OPPORTUNITYTableAdapter();
+        siteOpportunityTA.Fill(tableSITE_OPPORTUNITY);
+        
+        RetrofitsDataSetTableAdapters.PROJECTTableAdapter projectTA;
+        projectTA = new RetrofitsDataSetTableAdapters.PROJECTTableAdapter();
+        projectTA.Fill(tablePROJECT);
+
+        RetrofitsDataSetTableAdapters.PROJECT_STATUSTableAdapter projectStatusTA;
+        projectStatusTA = new RetrofitsDataSetTableAdapters.PROJECT_STATUSTableAdapter();
+        projectStatusTA.Fill(tablePROJECT_STATUS);
+
+        RetrofitsDataSetTableAdapters.SITETableAdapter siteTA;
+        siteTA = new RetrofitsDataSetTableAdapters.SITETableAdapter();
+        siteTA.Fill(tableSITE);
+
+        RetrofitsDataSetTableAdapters.DESTINATIONTableAdapter destinationTA;
+        destinationTA = new RetrofitsDataSetTableAdapters.DESTINATIONTableAdapter();
+        destinationTA.Fill(tableDESTINATION);
+
+        RetrofitsDataSetTableAdapters.FACILITY_TYPETableAdapter facilityTypeTA;
+        facilityTypeTA = new RetrofitsDataSetTableAdapters.FACILITY_TYPETableAdapter();
+        facilityTypeTA.Fill(tableFACILITY_TYPE);
+
+        RetrofitsDataSetTableAdapters.IMPERVIOUS_AREA_TYPETableAdapter imperviousAreaTypeTA;
+        imperviousAreaTypeTA = new RetrofitsDataSetTableAdapters.IMPERVIOUS_AREA_TYPETableAdapter();
+        imperviousAreaTypeTA.Fill(tableIMPERVIOUS_AREA_TYPE);
+
+        RetrofitsDataSetTableAdapters.OPPORTUNITY_FEASIBILITYTableAdapter opportunityFeasibilityTA;
+        opportunityFeasibilityTA = new RetrofitsDataSetTableAdapters.OPPORTUNITY_FEASIBILITYTableAdapter();
+        opportunityFeasibilityTA.Fill(tableOPPORTUNITY_FEASIBILITY);
+        
+        RetrofitsDataSetTableAdapters.RETRO_BASINTableAdapter retroBasinTA;
+        retroBasinTA = new RetrofitsDataSetTableAdapters.RETRO_BASINTableAdapter();
+        retroBasinTA.Fill(tableRETRO_BASIN); 
+
+        RetrofitsDataSetTableAdapters.SEWER_BASINTableAdapter sewerBasinTA;
+        sewerBasinTA = new RetrofitsDataSetTableAdapters.SEWER_BASINTableAdapter();
+        sewerBasinTA.Fill(tableSEWER_BASIN);
+      }
+    }
+}
+
+namespace SystemsAnalysis.DataAccess.RetrofitsDataSetTableAdapters {
+    
+    
+    public partial class SiteAssessmentTableAdapter {
+    }
+}
