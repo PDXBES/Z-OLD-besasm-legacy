a1ee8e0388b7fcade4987f3ed247e55083cbbf96
Arnel Mandilag <Arnel Mandilag@localhost>
2012-01-05 13:59:18 -0800

    Cost Estimator: Modified deprecated calls to AccessHelper's OpenDatabase function. Moved SystemsAnalysis.Utils.AccessUtils.DatabaseManagement project back to .NET 2.0 (it was at 3.50, probably in error). Forced x86 compile as Windows 7 usage of CostEstimator will fail if it tries to run 64-bit Jet engine (which doesn't exist).

--- DotNet/CostEstimator/Lib/ModelConstruction.ModelBuilder/ModelBuilder.cs
+++ DotNet/CostEstimator/Lib/ModelConstruction.ModelBuilder/ModelBuilder.cs
@@ -1,10 +1,7 @@
 using System;
-using System.Collections;
 using System.Collections.Generic;
-using System.Text;
 using SystemsAnalysis.ModelConstruction;
 using SystemsAnalysis.Utils.AccessUtils;
-using SystemsAnalysis.Utils.Events;
 
 namespace SystemsAnalysis.ModelConstruction
 {
@@ -21,14 +18,13 @@ namespace SystemsAnalysis.ModelConstruction
             ModelConfigurationDataSet modelConfigDS;
             modelConfigDS = new ModelConfigurationDataSet(modelRoot);
 
-            AccessHelper accessHelper = new AccessHelper();
 
             Dictionary<string, string> databases;
             databases = modelConfigDS.GetRequiredDatabases();
 
             foreach (string dbName in databases.Keys)
             {
-                accessHelper.OpenDatabase(databases[dbName]);
+              AccessHelper accessHelper = new AccessHelper(databases[dbName]);
 
                 Dictionary<string, string> linkedTableSources;
                 linkedTableSources = modelConfigDS.GetLinkedTableSources(dbName);
@@ -50,14 +46,12 @@ namespace SystemsAnalysis.ModelConstruction
             ModelConfigurationDataSet modelConfigDS;
             modelConfigDS = new ModelConfigurationDataSet(modelRoot);
 
-            AccessHelper accessHelper = new AccessHelper();
+            string dataAccessFileName = modelConfigDS.GetDataAccessFileName();
+            AccessHelper accessHelper = new AccessHelper(modelRoot + dataAccessFileName);
             try
             {
-                string dataAccessFileName = modelConfigDS.GetDataAccessFileName();
                 Dictionary<string, string[]> dataAccessTables;
                 dataAccessTables = modelConfigDS.GetDataAccessTables();
-
-                accessHelper.OpenDatabase(modelRoot + dataAccessFileName);
 
                 foreach (string linkName in dataAccessTables.Keys)
                 {
