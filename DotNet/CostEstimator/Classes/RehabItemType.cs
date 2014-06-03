2848b61fd90164e544e8e9eed51232a48291f6b1
Arnel Mandilag <Arnel Mandilag@localhost>
2014-06-03 15:32:47 -0700

    CostEstimator: Added RehabItemType and profiling

---
+++ DotNet/CostEstimator/Classes/RehabItemType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SystemsAnalysis.Analysis.CostEstimator.Classes
+{
+  enum RehabItemType
+  {
+    WholePipe,
+    Liner
+  }
+}
